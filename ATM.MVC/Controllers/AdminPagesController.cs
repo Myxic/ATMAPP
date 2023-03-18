de838efd9065217cdaaa46b9540444fbfd4881ab
Ezemenahi Benedict <ezemenahi56@gmail.com>
2023-03-19 00:49:57 +0100

    up

--- ATM.MVC/Controllers/AdminPagesController.cs
+++ ATM.MVC/Controllers/AdminPagesController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Diagnostics;
 using ATM.BLL.Interface;
+using ATM.BLL.Models;
+using ATM.DAL.Model;
 using ATM.MVC.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,11 +14,15 @@ namespace ATM.MVC.Controllers
     public class AdminPagesController : Controller
     {
         // GET: /<controller>/
-   private readonly ILogger<AdminPagesController> _logger;
+        private readonly ILogger<AdminPagesController> _logger;
+        private readonly ICustomerOperation _customerService;
+        private readonly IAdminOperations _adminService;
 
-           public AdminPagesController(ILogger<AdminPagesController> logger)
+        public AdminPagesController(ILogger<AdminPagesController> logger, ICustomerOperation customerOperation, IAdminOperations adminOperations)
            {
                _logger = logger;
+            _adminService = adminOperations;
+            _customerService = customerOperation;
            }
 
         public IActionResult Index()
@@ -31,20 +37,27 @@ namespace ATM.MVC.Controllers
 
         public IActionResult AddUser()
         {
-            return View();
+            
+            return View(new Customer());
         }
 
-        public IActionResult ViewComplains()
+        public async Task<IActionResult> ViewComplains()
         {
-            //var Model = 
+            var Model = await _adminService.GetComplainsAsync();
 
-            return View();
+            return View(Model);
         }
-        public IActionResult ViewWorkFlow()
+        public async Task<IActionResult> ViewWorkFlow(Admin admin)
         {
-            //var Model = 
-            return View();
+            var Model = await _adminService.GetAdminWorkFlowAsync(admin);
+
+            return View(Model);
         }
+
+        public IActionResult UpdateUser()
+        {
+            return View(new UpdateCustomer()); 
+	    }
 
         //[HttpGet]
         //public async Task<IActionResult> LoginValidation(IAdminOperations AdminOperation)
@@ -53,19 +66,88 @@ namespace ATM.MVC.Controllers
 
         //}
 
-        //[HttpPut]
-        //public async Task<IActionResult> Update()
-        //    {
+        [HttpPut]
+        public async Task<IActionResult> Update(UpdateCustomer model, Admin admin)
+        {
+            if (ModelState.IsValid)
+            {
 
-        // }
+                var (successful, msg) = await _adminService.UpdateCustomerRecordsAsync(admin, model);
+
+                if (successful)
+                {
+
+                    TempData["SuccessMsg"] = msg;
+
+                    return RedirectToAction("Index");
+                }
+
+                // TempData["ErrMsg"] = msg; for both views and redirect to actions
+
+                ViewBag.ErrMsg = msg;
+
+                return View("UpdateUser");
+
+            }
+
+            return View("UpdateUser");
+        }
 
 
-     //   [HttpPost]
-     //   public async Task<IActionResult> NewUser()
-     //   { 
-	    //}
+        [HttpPost]
+        public async Task<IActionResult> NewUser(Customer model, Admin admin)
+        {
+            if (ModelState.IsValid)
+            {
 
+                var (successful, msg) = await _adminService.CreateNewCustomerRecordAsync(admin, model);
 
+                if (successful)
+                {
+
+                    TempData["SuccessMsg"] = msg;
+
+                    return RedirectToAction("Index");
+                }
+
+                // TempData["ErrMsg"] = msg; for both views and redirect to actions
+
+                ViewBag.ErrMsg = msg;
+
+                return View("DeleteUserForm");
+
+            }
+
+            return View("DeleteUserForm");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(Admin admin, string model)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+                var (successful, msg) = await _adminService.DeleteCustomerRecordsAsync(admin, model);
+
+                if (successful)
+                {
+
+                    TempData["SuccessMsg"] = msg;
+
+                    return RedirectToAction("Index");
+                }
+
+                // TempData["ErrMsg"] = msg; for both views and redirect to actions
+
+                ViewBag.ErrMsg = msg;
+
+                return View("AddUser");
+
+            }
+
+            return View("AddUser");
+        }
 
         // GET: /<controller>/
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
